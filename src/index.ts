ddfafc8b58251262a4910ad33c07e0b77fe37fab
suhayb <ksuhiyp@gmail.com>
2023-11-24 13:24:53 +0300

    Activate CORS

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import { google } from "./script.js";
-
+import cors from "cors";
 const app: Express = express();
 dotenv.config();
 
-app.get("/", async (req, res) => {
+app.get("/", cors(), async (_, res) => {
   try {
-    await google();
-    res.send("selenium-webdriver script executed successfully");
+    const result = await google();
+    res.status(200).json({ result });
   } catch (error) {
     console.error("selenium script failed:\n", error);
     res.sendStatus(500).json({ error });
